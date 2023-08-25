ec6e7b1cdbe76a384810aa59dbca404577903f3e
Luciano Nascimento <luciano.m.nasc@gmail.com>
2023-08-25 19:26:48 -0300

    minute 40:00 youtube tutorial video

--- lua/luciano/plugins-setup.lua
+++ lua/luciano/plugins-setup.lua
@@ -52,6 +52,13 @@ return packer.startup(function(use)
   -- vs-code like icons
   use("nvim-tree/nvim-web-devicons")
 
+  -- statusline
+  use("nvim-lualine/lualine.nvim")
+
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+
+
   if packer_bootstrap then
     require('packer').sync()
   end
